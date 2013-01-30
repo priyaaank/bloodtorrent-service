d60352f3d99b4476e3639f029d900c69c8e5922d
Priyank Gupta <priyaaank@gmail.com>
2013-01-30 07:31:45 +0530

    Configured specs properly with database cleaner strategy for mongodb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,17 @@
 require 'mongoid'
-#require 'database_cleaner'
+require 'database_cleaner'
 
 Dir["models/**/*.rb"].sort.each {|file| require_relative "../#{file}" }
 
-#RSpec.configure do |config|
-  #config.before(:suite) do
-    #DatabaseCleaner[:mongoid].strategy = :truncation
-  #end
+ENV['RACK_ENV'] = 'test'
+Mongoid.load!("./config/mongoid.yml")
 
-  #config.after(:each) do
-    #DatabaseCleaner.clean
-  #end
-#end
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner[:mongoid].strategy = :truncation
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
