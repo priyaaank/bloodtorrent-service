34177757570761149db555311be9d872e09be2ef
Priyank Gupta <priyaaank@gmail.com>
2013-06-19 00:14:59 +1000

    Modified seed file to create indexes also and also fixed the latitude and longitude, which were swapped earlier

--- db/seed.rb
+++ db/seed.rb
@@ -2,6 +2,10 @@ require 'mongoid'
 require './models/donation_request'
 Mongoid.load!("./config/mongoid.yml")
 
-DonationRequest.create!(:blood_group => DonationRequest::BLOOD_GROUPS::O_POSITIVE, :quantity => 12, :coordinates => [18.5236, 73.8478], :requestor => "pg", :contact_details => "twitter")
-DonationRequest.create!(:blood_group => DonationRequest::BLOOD_GROUPS::A_POSITIVE, :quantity => 10, :coordinates => [12.9833, 77.5833], :requestor => "qg", :contact_details => "9929929929")
-DonationRequest.create!(:blood_group => DonationRequest::BLOOD_GROUPS::O_NEGATIVE, :quantity => 8, :coordinates => [29.0167, 77.3833], :requestor => "ap", :contact_details => "myemail@gmail.com")
+DonationRequest.delete_all
+
+DonationRequest.collection.indexes.create({ coordinates: "" }, { min: -200, max: 200 })
+
+DonationRequest.create!(:blood_group => DonationRequest::BLOOD_GROUPS::O_POSITIVE, :quantity => 12, :coordinates => [73.8478, 18.5236], :requestor => "pg", :contact_details => "twitter")
+DonationRequest.create!(:blood_group => DonationRequest::BLOOD_GROUPS::A_POSITIVE, :quantity => 10, :coordinates => [77.5833, 12.9833], :requestor => "qg", :contact_details => "9929929929")
+DonationRequest.create!(:blood_group => DonationRequest::BLOOD_GROUPS::O_NEGATIVE, :quantity => 8, :coordinates => [77.3833, 29.0167], :requestor => "ap", :contact_details => "myemail@gmail.com")
